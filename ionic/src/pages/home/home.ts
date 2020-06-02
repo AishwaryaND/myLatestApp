2291b97e268cd4bf13544f5ef9abb318ed49917c
AishwaryaND <39726899+AishwaryaND@users.noreply.github.com>
2020-06-02 17:12:33 +0530

    dcfv

--- ionic/src/pages/home/home.ts
+++ ionic/src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component, Renderer, NgZone } from "@angular/core";
 import { NavController, ModalController } from "ionic-angular";
-import { DataStore } from "../../app/dataStore";
 
 
 @Component({
